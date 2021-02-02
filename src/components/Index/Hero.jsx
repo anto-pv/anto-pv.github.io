afd8fb54aa8ec2a0304071d5fe26787b0919c4fd
anto-pv <antopv833@gmail.com>
2021-02-02 12:35:53 +0530

    added image

--- src/components/Index/Hero.jsx
+++ src/components/Index/Hero.jsx
@@ -30,6 +30,10 @@ const Hero = () => {
   return (
     <div>
     <section id="hero" className="jumbotron">
+      <div class="canvas black">
+	      <div class="top"></div>
+	      <div class="right"></div>
+      </div>
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
