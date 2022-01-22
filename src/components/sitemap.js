d3ad3f31a436d64d909be928f4e2a0fa1068b378
anto-pv <antopv833@gmail.com>
2022-01-22 19:34:08 +0530

    error test and year modification

--- src/components/sitemap.js
+++ src/components/sitemap.js
@@ -3,7 +3,7 @@ import { HashLink } from 'react-router-hash-link';
 
 const SiteMap = () => (
    <div className="sitemap">
-         <b>Anto PV@2021</b>
+         <b>Anto PV@2022</b>
          <ul><b><u>Site map</u></b>
             <HashLink className="sitelaink" to="/#header"><li>Home</li></HashLink>
             <HashLink className="sitelaink" to= "/portfolio#header"><li>Portfolio</li></HashLink>
